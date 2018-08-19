de6e9c20cbcd3a2efb58aab5d8f4ecb25dcf961a
rhard <rhard@mail.ru>
2018-08-19 12:49:33 +0200

    Linux build ready

--- conanfile.py
+++ conanfile.py
@@ -36,13 +36,10 @@ conan_basic_setup()''')
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="RakNet/Source")
+        self.copy("*raknet*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["RakNetLibStatic", "pthread"]
 
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-#include "hello.h"
+#include "RakPeer.h"
 
 int main() {
-    hello();
+    
+    RakNet::RakPeerInterface *server = RakNet::RakPeerInterface::GetInstance();
+    RakNet::SocketDescriptor socketDescriptor(0,0);
+    if (server->Startup(2, &socketDescriptor, 1) == RakNet::RAKNET_STARTED)
+        printf("RakServer started\n");
+    RakNet::RakPeerInterface::DestroyInstance(server);
+    return 0;
 }
